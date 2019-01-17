8ee70659e00ea02dca8bbf2e8aa6236597699c13
William Hua <william@attente.ca>
2019-01-17 00:00:31 -0500

    reproduce https://github.com/rustwasm/wasm-bindgen/issues/1191

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[cfg(feature = "bindings")]
+extern crate wasm_bindgen;
+#[cfg(feature = "bindings")]
+use wasm_bindgen::prelude::*;
+
+#[cfg_attr(feature = "bindings", wasm_bindgen)]
+pub struct Foo {
+}
+
+#[cfg_attr(feature = "bindings", wasm_bindgen)]
+impl Foo {
+    #[cfg_attr(feature = "bindings", wasm_bindgen(constructor))]
+    pub fn new() -> Foo {
+        Foo {}
     }
 }
